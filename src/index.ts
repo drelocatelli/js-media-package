ed37ef302eb17618ddcedc4d88283f543827f043
drelocatelli <you@example.com>
2022-09-08 15:36:42 -0300

    adding location

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export {useAudioRecorder} from './hooks/useAudioRecorder';
 export {useFileUpload} from './hooks/useFileUpload';
 export {useNotification} from './hooks/useNotification';
 export {useVideoRecorder} from './hooks/useVideoRecorder';
+export {useLocation} from './hooks/useLocation';
